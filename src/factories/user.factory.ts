c17202b361023b2f792f928231021f47486c135a
Guilherme Staciaki <gstaciaki99@gmail.com>
2026-01-31 13:19:55 -0300

    fix(factory): add missing parameter on user factory

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,9 +1,15 @@
 import { UserController } from "@src/controllers/user/user.controller";
 import { CreateUserUseCase } from "@src/use-cases/user/create-user/create-user.usecase";
+import { GetUserUseCase } from "@src/use-cases/user/get-user/get-user.usecase";
 import { ListUsersUseCase } from "@src/use-cases/user/list-users/list-users.usecase";
 
 export function makeUserController(): UserController {
   const createUserUseCase = new CreateUserUseCase();
   const listUsersUseCase = new ListUsersUseCase();
-  return new UserController(createUserUseCase, listUsersUseCase);
+  const getUserUseCase = new GetUserUseCase();
+  return new UserController(
+    createUserUseCase,
+    listUsersUseCase,
+    getUserUseCase,
+  );
 }
