60fab9de680fac377c08b9ed3edea20559ed09d3
Guilherme Staciaki <gstaciaki99@gmail.com>
2026-02-05 15:57:39 -0300

    feat(errors): translated texts to EN

--- src/errors/generic.errors.ts
+++ src/errors/generic.errors.ts
@@ -2,13 +2,13 @@ import { BaseBusinessError } from "./base-business.error";
 
 export class AlreadyExistsError extends BaseBusinessError {
   constructor(entity: string, field?: string) {
-    const fieldInfo = field ? ` com este(a) ${field}` : "";
-    super(`Já existe um(a) ${entity}${fieldInfo}.`);
+    const fieldInfo = field ? ` with this ${field}` : "";
+    super(`A ${entity} already exists${fieldInfo}.`);
   }
 }
 
 export class NotFoundError extends BaseBusinessError {
   constructor(entity: string, field: string = "id", value: string) {
-    super(`Não foi encontrado(a) ${entity} com ${field}: ${value}.`);
+    super(`No ${entity} was found with ${field}: ${value}.`);
   }
 }
